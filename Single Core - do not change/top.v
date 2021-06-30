4437145d45a43e115c7e6417ec4ba2da3c52ccac
Nipun Samarakoon <dewthilina1996@gmail.com>
2021-06-30 10:41:19 +0530

    4x4 matrices checked

--- Single Core - do not change/top.v
+++ Single Core - do not change/top.v
@@ -6,6 +6,18 @@ module top
     output wire [11:0] r2,
     output wire [11:0] r3,
     output wire [11:0] r4,
+	 output wire [11:0] r5,
+    output wire [11:0] r6,
+    output wire [11:0] r7,
+    output wire [11:0] r8,
+	 output wire [11:0] r9,
+    output wire [11:0] r10,
+    output wire [11:0] r11,
+    output wire [11:0] r12,
+	 output wire [11:0] r13,
+    output wire [11:0] r14,
+    output wire [11:0] r15,
+    output wire [11:0] r16,
     output wire end_process
 );
     wire [11:0] dm_out;
@@ -36,6 +48,8 @@ module top
     (
 	 .clk(clk),.write_en(dm_en),.addr(ar_out),.datain(bus_out),
     .dataout(dm_out),.r1(r1),.r2(r2),.r3(r3),.r4(r4)
+	 ,.r5(r5),.r6(r6),.r7(r7),.r8(r8),.r9(r9),.r10(r10),.r11(r11),.r12(r12)
+	 ,.r13(r13),.r14(r14),.r15(r15),.r16(r16)
     );
 
     instr_mem #(.width_in(12),.width_out(17)) im
--- Single Core - do not change/top_tb.v
+++ Single Core - do not change/top_tb.v
@@ -13,11 +13,26 @@ module top_tb;
     wire [11:0] r2;
     wire [11:0] r3;
     wire [11:0] r4;
+	 wire [11:0] r5;
+    wire [11:0] r6;
+    wire [11:0] r7;
+    wire [11:0] r8;
+	 wire [11:0] r9;
+    wire [11:0] r10;
+    wire [11:0] r11;
+    wire [11:0] r12;
+	 wire [11:0] r13;
+    wire [11:0] r14;
+    wire [11:0] r15;
+    wire [11:0] r16;
     wire end_process;
 	 reg start_process;
     // parameter N_reg = 12;
 
-    top top(.clk(clk),.start_process(start_process),.r1(r1),.r2(r2),.r3(r3),.r4(r4),
+    top top(.clk(clk),.start_process(start_process),.r1(r1),.r2(r2),.r3(r3),.r4(r4)
+	 ,.r5(r5),.r6(r6),.r7(r7),.r8(r8)
+	 ,.r9(r9),.r10(r10),.r11(r11),.r12(r12)
+	 ,.r13(r13),.r14(r14),.r15(r15),.r16(r16),
     .end_process(end_process));//later initiate this
 	
 initial begin
